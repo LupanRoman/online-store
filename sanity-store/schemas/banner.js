721f75c40d8e1e40568e8100967bc8a55c1785b2
LupanRoman <romalupan89@gmail.com>
2022-08-02 08:29:02 +0300

    first commit home page

---
+++ sanity-store/schemas/banner.js
@@ -0,0 +1,47 @@
+export default {
+    name: 'banner',
+    title: 'Banner',
+    type: 'document',
+    fields: [
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'image',
+        },
+        {
+            name: 'bigText',
+            title: 'BigText',
+            type: 'string',
+        },
+        {
+            name: 'buttonText',
+            title: 'ButtonText',
+            type: 'string',
+        },
+        {
+            name: 'mediumText',
+            title: 'MediumText',
+            type: 'string',
+        },
+        {
+            name: 'smallText',
+            title: 'SmallText',
+            type: 'string',
+        },
+        {
+            name: 'sale',
+            title: 'Sale',
+            type: 'string',
+        },
+        {
+            name: 'saleTime',
+            title: 'saleTime',
+            type: 'string',
+        },
+        {
+            name: 'details',
+            title: 'Details',
+            type: 'string',
+        },
+    ]
+}
